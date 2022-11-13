583f1a6dbe0327b9b6309d1322df92824370e82f
deepak9013216128 <deepak.kumar@createbytes.com>
2022-11-13 11:44:21 +0530

    create result apis

--- controllers/student.ts
+++ controllers/student.ts
@@ -1,6 +1,6 @@
 import { RequestHandler } from "express";
 import { ObjectId } from "mongodb";
-import { QuizInvitation, QuizQns } from "../models/quiz";
+import { QuizInvitation, QuizQns, UserRes } from "../models/quiz";
 
 export const getStudentQuiz: RequestHandler = async (req, res, next) => {
 	try {
@@ -89,6 +89,105 @@ export const submitQuiz: RequestHandler = async (req, res, next) => {
 		return res.status(200).json({
 			status: true,
 			quizInvitation,
+		});
+	} catch (err) {
+		next(err);
+	}
+};
+
+export const getQuizResults: RequestHandler = async (req, res, next) => {
+	try {
+		const results = await QuizInvitation.aggregate([
+			{
+				$match: {
+					invitedTo: new ObjectId(req.user),
+					isAttempted: true,
+				},
+			},
+			{
+				$lookup: {
+					from: "userres",
+					localField: "invitedTo",
+					foreignField: "user",
+					let: { invitedTo: "$invitedTo", quiz: "$quiz" },
+					pipeline: [
+						{
+							$match: {
+								$expr: {
+									$and: [
+										{ $eq: ["$user", "$$invitedTo"] },
+										{ $eq: ["$quiz", "$$quiz"] },
+									],
+								},
+							},
+						},
+						{ $project: { ans: 0, quiz: 0, user: 0 } },
+					],
+					as: "qns",
+				},
+			},
+			{
+				$lookup: {
+					from: "users",
+					localField: "invitedBy",
+					foreignField: "_id",
+					as: "invitedBy",
+				},
+			},
+			{
+				$lookup: {
+					from: "quizzes",
+					localField: "quiz",
+					foreignField: "_id",
+					as: "quiz",
+				},
+			},
+			{ $unwind: "$invitedBy" },
+			{ $unwind: "$quiz" },
+			{
+				$project: {
+					isAttempted: 1,
+					noOfQns: { $size: "$qns" },
+					invitedBy: {
+						name: 1,
+					},
+					successfullAttempted: {
+						$size: {
+							$filter: {
+								input: "$qns",
+								as: "qns",
+								cond: { $gt: ["$$qns.points", 0] },
+							},
+						},
+					},
+					qns: 1,
+					quiz: {
+						title: 1,
+						description: 1,
+						_id: 1,
+					},
+				},
+			},
+		]);
+		return res.status(200).json({
+			status: true,
+			results,
+		});
+	} catch (err) {
+		next(err);
+	}
+};
+
+export const getQuizResult: RequestHandler = async (req, res, next) => {
+	const { quizId } = req.params;
+	try {
+		const result = await UserRes.find({
+			user: new ObjectId(req.user),
+			quiz: new ObjectId(quizId),
+		}).populate("qns", "description options title");
+		return res.status(200).json({
+			status: true,
+			result,
 		});
 	} catch (err) {
 		next(err);
--- routes/student.ts
+++ routes/student.ts
@@ -1,6 +1,11 @@
 import { Router, Response, Request } from "express";
 
-import { getStudentQuiz, submitQuiz } from "../controllers/student";
+import {
+	getQuizResult,
+	getQuizResults,
+	getStudentQuiz,
+	submitQuiz,
+} from "../controllers/student";
 
 import { admin, protect } from "../middleware/auth";
 
@@ -11,5 +16,8 @@ router
 	.route("/quiz/:quizId")
 	.get(protect, getStudentQuiz)
 	.post(protect, submitQuiz);
+
+router.route("/results").get(protect, getQuizResults);
+router.route("/results/:quizId").get(protect, getQuizResult);
 
 export default router;
